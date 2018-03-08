30302e2c430b06becdff08d58c8d9aa9eb4c27c2
Boris Berdichevskiy <bberdich@gmail.com>
2018-03-08 15:42:21 -0500

    added spanId argument to update external count

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-function addOne() {
+window.widgetCount = 0;
+
+function handleClick(spanId) {
   widgetCount++;
+  console.log(spanId)
+  // const countSpan = document.getElementById(spanId);
+  spanId.innerHTML = widgetCount;
+
 }
 
-function insertCountWidget(divId) {
+function insertCountWidget(divId, spanId) {
   const counterDiv = document.getElementById(divId);
-  window.widgetCount = 0;
   counterDiv.innerHTML =
     `
     <div id="counter" style="background:lightgrey; text-align:center">
       <h1>
         Counter widget
       </h1>
-      <button id="counter-btn" style="background:lightblue" onclick="addOne()">
+      <button id="counter-btn" style="background:lightblue" onclick="handleClick(${spanId})">
         Count Up!
       </button>
     </div>
@@ -24,7 +29,7 @@ function insertCountWidget(divId) {
   //     <h1>
   //       Counter widget
   //     </h1>
-  //     <button id="counter-btn" style="background:lightblue" onclick="addOne()">
+  //     <button id="counter-btn" style="background:lightblue" onclick="handleClick()">
   //       Count Up!
   //     </button>
   //   </div>
